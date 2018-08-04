741a02a687a4034563785102986b4a6695b27100
monterocjaime <monterocjaime@gmail.com>
2018-08-04 11:30:03 -0400

    Created function for routing html, and exporting

--- routes/data.js
+++ routes/data.js
@@ -1,16 +1,16 @@
 var dataTables = require("../data/table");
 var dataWaitList = require("../data/waitlist");
 
-module.exports = function(hotRes){
-    hotRes.get("/api/tables", function(req, res) {
+module.exports = function(app){
+    app.get("/api/tables", function(req, res) {
         res.json(dataTables);
       });
 
-      hotRes.get("/api/waitlist", function(req, res) {
+      app.get("/api/waitlist", function(req, res) {
         res.json(dataWaitList);
       });
     
-      hotRes.post("/api/tables", function(req, res) {
+      app.post("/api/tables", function(req, res) {
     
         if (dataTables.length < 8) {
           dataTables.push(req.body);
